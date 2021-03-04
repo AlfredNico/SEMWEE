8777acdd4dfdda3c0b1d5e92fc2de5937c3a82f6
AlfredNico <alfrednicotsu@gmail.com>
2021-03-04 07:52:46 +0300

    Session login ok

--- src/app/authentification/components/sign-in/sign-in.component.ts
+++ src/app/authentification/components/sign-in/sign-in.component.ts
@@ -2,6 +2,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { User } from '@app/classes/users';
 import { ReCapchaService } from '@app/services/re-capcha.service';
 import { CookieService } from 'ngx-cookie-service';
 import { AuthService } from '../../services/auth.service';
@@ -23,7 +24,7 @@ export class SignInComponent implements OnInit {
 
 
   constructor(private authService: AuthService, private fb: FormBuilder,
-    private cookie: CookieService,private router: Router, public recaptcha: ReCapchaService) { }
+    private cookie: CookieService, private router: Router, public recaptcha: ReCapchaService) { }
 
   get getusername() { return this.loginForm.controls.username; }
   get getpassword() { return this.loginForm.controls.password; }
@@ -35,17 +36,12 @@ export class SignInComponent implements OnInit {
 
   public async onSubmit() {
     const { username, password }: { [key: string]: any } = this.loginForm.controls;
-    
+
     try {
-      const user = await this.authService.login({ username: username.value, password: password.value});
+      const user = await this.authService.login({ username: username.value, password: password.value });
       if (user) {
         if (this.stayOn.value === true) {
-          this.cookie.set('id', JSON.stringify(user.id), 0.2, '/', 'semewee', true, 'Strict');
-          this.cookie.set('semewee', user.token, 0.2, '/', 'semewee', true, 'Strict');
-          this.cookie.set('firstName', user.firstName, 0.2, '/', 'semewee', true, 'Strict');
-          this.cookie.set('lastName', user.lastName, 0.2, '/', 'semewee', true, 'Strict');
-          this.cookie.set('password', user.password, 0.2, '/', 'semewee', true, 'Strict');
-          this.cookie.set('password', user.username, 0.2, '/', 'semewee', true, 'Strict');
+          localStorage.setItem('currentUser', JSON.stringify(user));
         }
 
         this.router.navigateByUrl('/espace-user');
