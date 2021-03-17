2f2b7077a34288fcbe6a8634ab44e19efe8f9d23
AlfredNico <alfrednicotsu@gmail.com>
2021-03-17 14:04:42 +0300

    Updates server HEROKU

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
-//Install express server
 const express = require('express');
+const app = express();
 const path = require('path');
 
-const app = express();
+app.use(express.static(__dirname + '/dist'));
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/semwee'));
-
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/semwee/index.html'));
-});
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
 
+//Path Location Strategy
 
-// // ngApp.listen(process.env.PORT || 8080);
-// const express = require('express');
-// const app = express();
-// // Run the app by serving the static files
-// // in the dist directory
-// // app.use(express.static(__dirname + '/dist'));
-// app.use(express.static(__dirname + '/dist/index.html'));
-// // Start the app by listening on the default
-// // Heroku port
-// app.listen(process.env.PORT || 8080);
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
+
+console.log('Console listening!');
