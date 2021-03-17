24e91abfa172d731bb86550c20d7cde52d6e2a88
AlfredNico <alfrednicotsu@gmail.com>
2021-03-17 13:43:20 +0300

    Updates columns rows

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
-// const express = require('express');
-// const path = require('path');
-// const ngApp = express();
-// ngApp.use(express.static('./dist/SEMWEE'));
-// ngApp.get('/*', function(request, response) {
-//     response.sendFile(path.join(__dirname, '/dist/SEMWEE/index.html'));
-// });
-// ngApp.listen(process.env.PORT || 8080);
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-// Run the app by serving the static files
-// in the dist directory
-// app.use(express.static(__dirname + '/dist'));
-app.use(express.static(__dirname + '/dist/index.html'));
-// app.get('*', function(req, res) {
-//     res.sendfile(__dirname + './dist/index.html'); // load our index.html file
-// });
-// Start the app by listening on the default
-// Heroku port
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/semwee'));
+
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/semwee/index.html'));
+});
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
+
+// // ngApp.listen(process.env.PORT || 8080);
+// const express = require('express');
+// const app = express();
+// // Run the app by serving the static files
+// // in the dist directory
+// // app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist/index.html'));
+// // Start the app by listening on the default
+// // Heroku port
+// app.listen(process.env.PORT || 8080);
