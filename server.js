b580e178c6292b123f2cd5b208cb3982a1f34e37
AlfredNico <alfrednicotsu@gmail.com>
2021-03-17 11:30:08 +0300

    updates routin angular

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist/index.html'));
+app.get('*', function(req, res) {
+    res.sendfile(__dirname + './dist/index.html'); // load our index.html file
+});
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
