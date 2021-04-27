c588c45cad97c5542884357a2be3622346e6c46b
Fandresena <fahtialalaina2@gmail.com>
2021-04-27 18:30:29 +0300

    fix tune it back-front

--- src/app/user-spaces/dashbord/services/lp-validator.service.ts
+++ src/app/user-spaces/dashbord/services/lp-validator.service.ts
@@ -116,7 +116,6 @@ export class LpValidatorService {
             'Item_Type',
             'List_Page_Label',
             'List_Page_Main_Query',
-            'Item_Type',
             '_1st_Property',
             '_2nd_Property',
             '_3rd_Property',
