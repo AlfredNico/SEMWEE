4447f8ce1c7caee73c5197debfc193587d97cfcd
Fandresena <fahtialalaina2@gmail.com>
2021-05-14 17:53:26 +0300

    remove button refine

--- src/app/user-spaces/dashbord/components/lp-viewer/facet-filter.component.ts
+++ src/app/user-spaces/dashbord/components/lp-viewer/facet-filter.component.ts
@@ -97,11 +97,6 @@ import { LpViwersService } from '../../services/lp-viwers.service';
       <p class="m-0">
         Use facets and filters to select subsets of your data to act on. Choose facet and filter methods from the menus at the top of each data column.
       </p>
-      <p class="m-0">Not sure how to get started?<br>
-      <a href="https://github.com/OpenRefine/OpenRefine/wiki/Screencasts" target="_blank">
-        <b>Watch these screencasts</b>
-      </a>
-    </p>
   </div>
   </ng-template>
   `,
